7e5476f9e0a6a5ea5fcd23320d45512c60ee3a45
Rodrigo Del Angel Gerardo <20210658@uthh.edu.mx>
2023-03-25 10:17:50 -0600

    api actualizada diez

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -129,6 +129,7 @@ export const imagen = async (req, res) =>{
         return res.status(200).json({img})
     } catch (error) {
         console.log(error);
+        return res.status(500).json({error})
     }
 }
 
